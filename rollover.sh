9af4ceec0138658a5cf1c29b45f8e056e44ff797
forselli-stratio <forselli@stratio.com>
2019-09-30 14:10:05 +0200

    Both scripts will run in background

--- rollover.sh
+++ rollover.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-	curl -k --cacert ./ca.crt --cert ./elasticlogrotate.pem --key ./elasticlogrotate.key -X POST "$ELASTIC_HOST:$ELASTIC_PORT/$INDEX_NAME/_rollover" -H 'Content-Type: application/json' -d'{"conditions": {"max_age": "${MAX_AGE_ROLLOVER}","max_docs":${MAX_DOCS_ROLLVER},"max_size": "${MAX_SIZE_ROLLOVER}"}}'
+	curl -k --cacert ./ca.crt --cert ./elasticlogrotate.pem --key ./elasticlogrotate.key -X POST "$ELASTIC_HOST:$ELASTIC_PORT/$INDEX_NAME/_rollover" -H 'Content-Type: application/json' -d'{"conditions": {"max_age": "${MAX_AGE_ROLLOVER}","max_docs":${MAX_DOCS_ROLLOVER},"max_size": "${MAX_SIZE_ROLLOVER}"}}'
     sleep $ROLLOVERSCRIPT_TIME
 done
